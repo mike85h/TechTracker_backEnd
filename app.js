619e076c17ac5213b14f2e9df0757eb4ebed9928
Michael Harrington <mike85h@gmail.com>
2017-07-24 15:35:45 -0600

    dependencies added

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 const express = require('express');
 const knex = require('knex');
+const app = express();
+const port = port.env.PORT || 8080;
+
+app.get('/', function(req, res){
+  res.send("hello world");
+})
+
+
+
+
+
+
+app.listen(port);
