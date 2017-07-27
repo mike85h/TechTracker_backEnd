a801940ae8a574745a7970eff7365eaac5e691da
Michael Harrington <mike85h@gmail.com>
2017-07-27 12:53:49 -0600

    heroku deploy

--- db/knex.js
+++ db/knex.js
@@ -1,16 +1,3 @@
-// const environment = process.env.NODE_ENV || 'development';
-// const config = require('../knexfile.js')[environment];
-//
-// module.exports = require('knex')(config);
-
-
-// var config      = require('../knexfile.js');
-// var env         = process.env.NODE_ENV || 'development';
-// var knex        = require('knex')(config[env]);
-//
-// module.exports = knex;
-//
-// knex.migrate.latest([config]);
  var knex = require('knex')
  var configs = require('../knexfile')
  var environment = 'development'
