d8f49b28f1b18cbe08267fc872fc3e12ecddfce7
Michael Harrington <mike85h@gmail.com>
2017-07-27 14:32:02 -0600

    ok

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const knex = require('./db/knex.js')
 const app = express()
 const port = process.env.PORT || 8080
 const cors = require('cors')
+const bodyParser = require('body-parser')
 
 app.use(cors())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function(req, res){
   knex('technology').select('*').then(function(technologies){
